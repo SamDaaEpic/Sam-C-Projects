ba8c594931b1a53bb5b56915e76248662d57a121
Sameer9793 <SamTheEpic@gmail.com>
2021-09-27 16:10:45 +0530

    My first ever C program

---
+++ AgeToYearCoverter.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int age;
+    int year = 2021;
+    printf("This program will tell you your birth of year, you just have to enter in your age below.\n");
+    printf("Whats your age? Enter here:");
+    scanf("%d",&age);
+    int BirthYear = year - age;
+    printf("You was born in %d year", BirthYear);
+
+
+     return 0;
+}
